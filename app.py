606ae1beacc0cb9ba14d408b65be0917fe16034c
Tasneem-I <107906533+Tasneem-I@users.noreply.github.com>
2023-05-03 12:23:59 +0530

    template file creation, signup form and backend
    1. Set up routes on the backend for signup, login, dashboard, todos and delete todo
    2. Created corresponding file templates for the routes
    3. Worked on responsive base version of signup template

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 from flask_session import Session
 from sqlalchemy import func
 import os
@@ -15,7 +15,7 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-class Users(UserMixin, db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(50), nullable= False)
     name = db.Column(db.String(60), nullable=False)
@@ -28,7 +28,67 @@ class Todo(db.Model):
     title = db.Column(db.String(50), nullable= False)
     content = db.Column(db.String(3000))
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 db.init_app(app)
 with app.app_context():
     db.create_all()
+
+@login_manager.user_loader
+def user_load(user_id):
+    return User.query.get(user_id)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User(username=username, name=name, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return render_template("login.html")
+    else:
+        return render_template("signup.html")
+    
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = User.query.filter_by(email = request.form.get("email")).first()
+        if user.password == request.form.get("password"):
+            login_user()
+            return redirect(url_for("dashboard"))
+    else:
+        return render_template("login.html")
+    
+
+@app.route("/dashboard", methods= ["GET", "POST"])
+def dash():
+    return render_template("dashboard.html")
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        user= current_user()
+        todo = Todo(title = request.form.get("title"), content = request.form.get("content"), user_id = user.id)
+        db.session.add(todo)
+        db.session.commit()
+        return redirect(url_for("todos"))
+    else:
+        todos = Todo.query.filter_by(user_id = current_user.id)
+        return render_template("todos.html", todos = todos)
+
+@app.route("/delete/<int:id>", methods=["GET", "POST"])
+def delete(id):
+    task = Todo.query.filter_by(user_id = current_user.id, id = id)
+    db.session.delete(id)
+    db.session.commit()
+    return render_template('todos.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
